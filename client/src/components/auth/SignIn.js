f0faa43ac26fad3d61041118191e9eb635a1fd36
Zakaria Moshi <zkemoshi@gmail.com>
2021-11-01 19:00:48 +0300

    signin style

--- client/src/components/auth/SignIn.js
+++ client/src/components/auth/SignIn.js
@@ -9,20 +9,41 @@ import {
   Button,
   CssBaseline,
   TextField,
-  FormControlLabel,
-  Checkbox,
   Grid,
   Box,
   Typography,
   Container,
+  Paper,
+  Divider,
+  Chip,
 } from '@mui/material';
-
+import { makeStyles } from '@material-ui/core';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import EmailIcon from '@mui/icons-material/Email';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import logo from '../layout/logo.png';
 
 const theme = createTheme();
 
+const useStyles = makeStyles({
+  logo: {
+    padding: '0',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 3,
+  },
+  contact: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginTop: '1rem',
+  },
+});
+
 export default function SignIn() {
+  const classes = useStyles();
   const alertContext = useContext(AlertContext);
   const authContext = useContext(AuthContext);
   const history = useHistory();
@@ -60,9 +81,29 @@ export default function SignIn() {
     <ThemeProvider theme={theme}>
       <Container component='main' maxWidth='xs'>
         <CssBaseline />
+        <Grid container sx={{ mt: 2 }} alignItems='center'>
+          <Grid item xs={6}>
+            <Typography className={classes.logo}>
+              <img src={logo} alt='logo' width={150} />
+            </Typography>
+          </Grid>
+          <Grid item xs={6} align='right'>
+            <Button variant='outlined' color='warning'>
+              GET STARTED
+            </Button>
+          </Grid>
+        </Grid>
+        <Typography
+          align='center'
+          variant='h2'
+          className={classes.title}
+          sx={{ color: '#ef964c', fontSize: '34px' }}
+        >
+          Manage Your Lodge on The Go
+        </Typography>
         <Box
           sx={{
-            marginTop: 7,
+            marginTop: 4,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
@@ -132,6 +173,47 @@ export default function SignIn() {
             </Grid>
           </Box>
         </Box>
+        <Grid className='details' sx={{ mt: 3 }}>
+          <Paper sx={{ p: 2, bgcolor: '#b154c4', color: 'white' }}>
+            <Typography align='center'>
+              Our service will enable you to easily get notified when a customer
+              book a room.
+            </Typography>
+          </Paper>
+          <Paper sx={{ mt: 2, p: 2, bgcolor: '#3A9AC5', color: 'white' }}>
+            <Typography align='center'>
+              Get detailed report to assist in making informed business decision
+            </Typography>
+          </Paper>
+        </Grid>
+        <Button
+          fullWidth
+          variant='outlined'
+          color='warning'
+          sx={{ mt: 2.5, mb: 3.5 }}
+        >
+          GET STARTED
+        </Button>
+        <Divider>
+          <Chip color='primary' label='Contact Us' />
+        </Divider>
+        <Grid sx={{ mb: 5 }} className={classes.contact}>
+          <Chip
+            label='+255755059683'
+            component='a'
+            href='#basic-chip'
+            variant='outlined'
+            clickable
+            color='primary'
+            icon={<WhatsAppIcon />}
+          />
+          <Chip
+            icon={<EmailIcon />}
+            label='info@lodge.co.tz'
+            variant='outlined'
+            color='warning'
+          />
+        </Grid>
       </Container>
     </ThemeProvider>
   );
